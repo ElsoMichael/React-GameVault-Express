ca5e2e6452fada91897c6c79ea345ec515b32c9c
ElsoMichael <elsomichael@ymail.com>
2018-05-15 20:43:49 -0400

    Bug Fixing

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var userGames = require('./routes/user-games');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/user-games', userGames);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
